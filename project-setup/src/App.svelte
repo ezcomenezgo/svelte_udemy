e48e43eea981771e44ed9ad476316dca6b7ec4cb
Claire <s3310507057@gmail.com>
2021-06-15 23:43:09 +0800

    20210615

---
+++ project-setup/src/App.svelte
@@ -0,0 +1,64 @@
+<script>
+  import ContactCard from './ContactCard.svelte'
+
+  let name = 'Max'
+  let title = ''
+  let image = ''
+  let description = ''
+  let formState = 'empty'
+
+  function addContact() {
+    if (
+      name.trim().length === 0 ||
+      title.trim().length === 0 ||
+      image.trim().length === 0 ||
+      description.trim().length === 0
+    ) {
+      formState = 'invalid'
+      return
+    } else {
+      formState = 'done'
+    }
+  }
+</script>
+
+<div id="form">
+  <div class="form-control">
+    <label for="userName">User Name</label>
+    <input type="text" bind:value={name} id="userName" />
+  </div>
+  <div class="form-control">
+    <label for="jobTitle">Job Title</label>
+    <input type="text" bind:value={title} id="jobTitle" />
+  </div>
+  <div class="form-control">
+    <label for="image">Image URL</label>
+    <input type="text" bind:value={image} id="image" />
+  </div>
+  <div class="form-control">
+    <label for="desc">Description</label>
+    <textarea rows="3" bind:value={description} id="desc" />
+  </div>
+</div>
+
+<button on:click={addContact}>add Contact Card</button>
+
+{#if formState === 'done'}
+  <ContactCard
+    userName={name}
+    jobTitle={title}
+    {description}
+    userImage={image}
+  />
+{:else if formState === 'invalid'}
+  <p>input invalid</p>
+{:else}
+  <p>Please fill some data and hit the button!</p>
+{/if}
+
+<style>
+  #form {
+    width: 30rem;
+    max-width: 100%;
+  }
+</style>
