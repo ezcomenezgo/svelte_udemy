c901fe9209ebffe59b3b9d48abbf4a78a8d2b5da
Claire <s3310507057@gmail.com>
2021-06-23 11:46:42 +0800

    Accessing Windows, Body, Head

--- starting-project-L16/src/App.svelte
+++ starting-project-L16/src/App.svelte
@@ -3,6 +3,11 @@
   import CartItem from './CartItem.svelte'
   import FamilyNode from './FamilyNode.svelte'
 
+  let y
+  let title = 'My app'
+
+  $: console.log(y)
+
   let familyStructure = [
     {
       isParent: true,
@@ -16,6 +21,12 @@
       ],
     },
     { isParent: true, name: 'Danny' },
+    { isParent: true, name: 'Danny' },
+    { isParent: true, name: 'Danny' },
+    { isParent: true, name: 'Danny' },
+    { isParent: true, name: 'Danny' },
+    { isParent: true, name: 'Danny' },
+    { isParent: true, name: 'Danny' },
   ]
 
   let renderedComponent = { cmp: Product, title: 'Test Product', id: 'p1' }
@@ -27,16 +38,35 @@
       renderedComponent = { cmp: Product, title: 'Test Product', id: 'p1' }
     }
   }
+
+  function switchTitle() {
+    title = 'A new title'
+  }
 </script>
 
-<button on:click={toggle}>Toggle Display</button>
+<svelte:window bind:scrollY={y} />
+<svelte:body on:mouseenter />
+<svelte:head>
+  <title>{title}</title>
+</svelte:head>
 
-<svelte:component
-  this={renderedComponent.cmp}
-  title={renderedComponent.title}
-  id={renderedComponent.id}
-/>
+<button on:click={switchTitle}>Switch</button>
+<div class="high">
+  <button on:click={toggle}>Toggle Display</button>
 
-{#each familyStructure as familyMember}
-  <FamilyNode member={familyMember} />
-{/each}
+  <svelte:component
+    this={renderedComponent.cmp}
+    title={renderedComponent.title}
+    id={renderedComponent.id}
+  />
+
+  {#each familyStructure as familyMember}
+    <FamilyNode member={familyMember} />
+  {/each}
+</div>
+
+<style>
+  div {
+    height: 5000px;
+  }
+</style>
